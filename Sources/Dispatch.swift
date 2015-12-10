c3c81fd6982387c6f089a368e03cb66dc2f696a4
John Holdsworth <github@johnholdsworth.com>
2015-12-10 05:00:18 +0000

    DISPATCH_QUEUE_PRIORITY_BACKGROUND

--- Sources/Dispatch.swift
+++ Sources/Dispatch.swift
@@ -5,7 +5,7 @@
 //  Created by John Holdsworth on 11/06/2015.
 //  Copyright (c) 2015 John Holdsworth. All rights reserved.
 //
-//  $Id: //depot/NSLinux/Sources/Dispatch.swift#6 $
+//  $Id: //depot/NSLinux/Sources/Dispatch.swift#7 $
 //
 //  Repo: https://github.com/johnno1962/NSLinux
 //
@@ -15,7 +15,7 @@
 #if os(Linux)
 import Glibc
 
-public let DISPATCH_QUEUE_CONCURRENT = 0, DISPATCH_QUEUE_PRIORITY_HIGH = 0, DISPATCH_QUEUE_PRIORITY_LOW = 0
+public let DISPATCH_QUEUE_CONCURRENT = 0, DISPATCH_QUEUE_PRIORITY_HIGH = 0, DISPATCH_QUEUE_PRIORITY_LOW = 0, DISPATCH_QUEUE_PRIORITY_BACKGROUND = 0
 
 public func dispatch_get_global_queue( type: Int, _ flags: Int ) -> Int {
     return type
